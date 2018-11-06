b415717c0275e87173010317b2d3413ceb3c4575
汤然 <ran.tang@westwell-lab.com>
2018-11-06 20:56:11 +0800

    triangulate unseen points

--- main.cpp
+++ main.cpp
@@ -57,6 +57,7 @@ void Triangulate(const cv::Point2f &pt1, const cv::Point2f &pt2, const cv::Mat &
     cv::SVD::compute(A, w, u, vt);
     X = vt.row(3).t();
     X = X.rowRange(0,3) / X.at<double>(3);
+    X = X / cv::norm(X);
 }
 
 int CheckGoodPoint(const std::vector<cv::Point2f> &pt1, const std::vector<cv::Point2f> &pt2, const cv::Mat &K,
@@ -89,6 +90,121 @@ int CheckGoodPoint(const std::vector<cv::Point2f> &pt1, const std::vector<cv::Po
     return num_goods;
 }
 
+void DecomposeEssentialMatrix(const std::vector<cv::Point2f> &pt1, 
+                        const std::vector<cv::Point2f> &pt2, 
+                        const cv::Mat &E, 
+                        const cv::Mat &K, 
+                        cv::Mat &R,
+                        cv::Mat &t) {
+    // svd
+    cv::Mat u,w,vt;
+    cv::SVD::compute(E, w, u, vt);
+    // std::cout << "u: " << u*u.t() << std::endl;
+    // std::cout << "u determinant: " << cv::determinant(u) << std::endl;
+    // std::cout << "vt: " << vt.t() * vt << std::endl;
+    // std::cout << "vt determinant: " << cv::determinant(vt) << std::endl;
+    // std::cout << "w: " << w << std::endl;
+
+    // check equality
+    double ratio = fabs(w.at<double>(0) / w.at<double>(1));
+    if (ratio > 1.0) ratio = 1.0 / ratio;
+    if (ratio <0.7) {
+        std::cout << "sigular values are too far away: " << ratio << std::endl;
+        return;
+    } 
+    // std::cout << "ratio: " << ratio << std::endl;
+    cv::Mat W = (cv::Mat_<double>(3,3) << 0, -1, 0, 1, 0, 0, 0, 0, 1);
+    cv::Mat R0 = u * W * vt;
+    if (cv::determinant(R0) < 0)
+        R0 = -R0;
+    cv::Mat R1 = u * W.t() * vt;
+    if (cv::determinant(R1) < 0)
+        R1 = -R1;
+    cv::Mat t0 = u.col(2);
+    cv::Mat t1 = -u.col(2);
+
+    // test four solutions
+    cv::Mat P0 = cv::Mat::eye(3,4,R0.type());
+    std::vector<cv::Mat> candidates;
+    for (int i=0; i<4; i++)
+        candidates.push_back(cv::Mat(3,4,R0.type()));
+    R0.copyTo(candidates[0].colRange(0,3));t0.copyTo(candidates[0].col(3));
+    R0.copyTo(candidates[1].colRange(0,3));t1.copyTo(candidates[1].col(3));
+    R1.copyTo(candidates[2].colRange(0,3));t0.copyTo(candidates[2].col(3));
+    R1.copyTo(candidates[3].colRange(0,3));t1.copyTo(candidates[3].col(3));
+    
+    // cv::Mat P1(3,4,R0.type()), P2(3,4,R0.type()), P3(3,4,R0.type()), P4(3,4,R0.type());    
+    // std::cout << "R0: " << R0 << std::endl;
+    // std::cout << "R1: " << R1 << std::endl;
+    // std::cout << "t: " << t0 << std::endl;
+    // std::cout << "P1: " << candidates[0] << std::endl;
+    // std::cout << "P2: " << candidates[1] << std::endl;
+    // std::cout << "P3: " << candidates[2] << std::endl;
+    // std::cout << "P4: " << candidates[3] << std::endl;
+    
+    std::vector<int> num_good(4,0);
+    for (int i=0; i<4; i++) {            
+        num_good[i] = CheckGoodPoint(pt1, pt2, K, P0, candidates[i]);
+        // std::cout << "num_good" << i << ": " << num_good[i] << std::endl;
+    }
+
+    std::vector<int>::iterator result = std::max_element(num_good.begin(), num_good.end());
+    int max_position = std::distance(num_good.begin(), result);
+    R = candidates[max_position].colRange(0,3).clone();
+    t = candidates[max_position].col(3).clone();
+
+    // cv::Mat R, trans;
+    // double focal = 517.3;
+    // cv::Point2d pp(318.6, 255.3); 
+    // cv::recoverPose(E, pt1, pt2, R, trans, focal, pp);
+    // cv::Mat gt(3, 4, R.type());
+    // gt.colRange(0,3) = R * 1.0;
+    // gt.col(3) = trans * 1.0;
+    // std::cout << "estimated: " << camera_matrix << std::endl;
+    // std::cout << "gt: " << gt << std::endl;
+}
+
+void TriangulateUnseenPoints(const std::vector<cv::Point2f> &pt1,
+                            const std::vector<cv::Point2f> &pt2,
+                            std::vector<cv::Point3f> &point_cloud, 
+                            const cv::Mat &last_pose,
+                            const cv::Mat &current_pose,
+                            const cv::Mat &K,
+                            int widht,
+                            int height) {
+    // reproject
+    std::vector<cv::Point2f> reproject_points;
+
+    for (int i=0; i<point_cloud.size(); i++) {
+        cv::Mat homogeneous_point = (cv::Mat_<double>(4, 1) << point_cloud[i].x, point_cloud[i].y, point_cloud[i].z, 1.0);
+        std::cout << "homogeneous_point : " << point_cloud[i] << std::endl;
+        std::cout << "homogeneous_point : " << homogeneous_point << std::endl;
+        cv::Mat reproject_point = K * current_pose.rowRange(0,3) * homogeneous_point;
+        reproject_point = reproject_point.rowRange(0,3) / reproject_point.at<double>(3);    
+        reproject_points.push_back(cv::Point2f(reproject_point.at<double>(0), reproject_point.at<double>(1)));  
+        std::cout << "reproject point: " << reproject_point << std::endl;          
+    }
+       
+    std::vector<bool> unseen(pt2.size(), true);
+    for (int i=0; i<pt2.size(); i++) {
+        for (int j=0; j<reproject_points.size(); j++) {    
+            double dist = cv::norm(pt2[i] - reproject_points[j]);
+            if (dist < 2) {
+                unseen[i] = false;
+                break;
+            }                
+        }                
+    }
+
+    for (int i=0; i<pt2.size(); i++) {
+        if (!unseen[i])
+            continue;
+        cv::Mat X;
+        Triangulate(pt1[i], pt2[i], last_pose, current_pose, X);
+        point_cloud.push_back(cv::Point3f(X.at<double>(0), X.at<double>(1), X.at<double>(2)));
+    }
+}
+
 int main (int argc, char **argv) {
     cv::VideoCapture cap("../videos/test_freiburgxyz525.mp4");
     if (!cap.isOpened()) {
@@ -107,23 +223,39 @@ int main (int argc, char **argv) {
     cv::Mat K = (cv::Mat_<double>(3,3)<<517.3, 0.0, 318.6, 0.0, 516.5, 255.3, 0.0, 0.0, 1);
     std::cout << "K" << K << std::endl;
 
+    std::vector<cv::Mat> pose_history;
+    std::vector<cv::Point3f> point_cloud;
+    std::vector<cv::Mat> descriptor_history;
+    std::vector<std::vector<cv::KeyPoint>> key_point_history;
+    cv::Mat last_pose(4,4,CV_64F);
+    int width, height;
+
     while (true) {
         cv::Mat original_frame;
         cv::Mat frame;
+        cv::Mat current_pose = cv::Mat::eye(4, 4, CV_64F);
         std::vector<cv::KeyPoint> key_points;
         cv::Mat descriptors;
         std::vector<std::vector<cv::DMatch> > matches;
 
         cap >> original_frame;
-        frame = original_frame.clone();
         // std::cout << "frame size: " << frame.size() << std::endl;
         // cv::resize(original_frame, frame, cv::Size(), 0.5, 0.5);
+        frame = original_frame.clone(); 
         orb->detectAndCompute(frame, cv::Mat(), key_points, descriptors);
+        
         if (first_frame) {
             first_frame = false;
             last_frame = frame;
             last_descriptors = descriptors;
-            last_key_points = key_points;
+            last_key_points = key_points; 
+            pose_history.push_back(current_pose.clone());
+            descriptor_history.push_back(descriptors.clone());
+            key_point_history.push_back(key_points);
+            last_pose = current_pose.clone();
+            cv::Size frame_size = frame.size();
+            width = frame_size.width;
+            height = frame_size.height;
             continue;
         }
 
@@ -138,30 +270,7 @@ int main (int argc, char **argv) {
             points_1.push_back(last_key_points[matches[i][0].queryIdx].pt);
             points_2.push_back(key_points[matches[i][0].trainIdx].pt);
         }
-
-        // check inliers
-        // for (int i=0; i<matches.size(); i++) {
-        //     int num_matches = static_cast<int>(matches[i].size());
-        //     double min_dist = matches[i][0].distance;
-        //     // std::cout << "min distance: " << min_dist << std::endl;
-        //     if (min_dist < min_threshold) {
-        //         inlier_matches.push_back(matches[i][0]);
-        //         inlier_1.push_back(last_key_points[matches[i][0].queryIdx]);
-        //         inlier_2.push_back(key_points[matches[i][0].trainIdx]);
-        //     }
-            
-        // }
         
-        // std::cout << "match size: " << matches.size() << std::endl;
-        // std::cout << "inlier matches size : " << inlier_matches.size() << std::endl;
-
-        // cv::KeyPoint::convert(inlier_1, points_1);
-        // cv::KeyPoint::convert(inlier_2, points_2);
-        
-        // for (int i=0; i<inlier_1.size(); i++) {
-        //     std::cout << "points 1 : " << inlier_1[i].pt << std::endl;
-        //     std::cout << "points 2 : " << inlier_2[i].pt << std::endl;
-        // }
         std::vector<uchar> status(points_1.size());
         cv::Mat F = cv::findFundamentalMat(points_1, points_2, CV_FM_RANSAC, 2, 0.99, status);
 
@@ -174,9 +283,6 @@ int main (int argc, char **argv) {
                 inlier_2.push_back(points_2[i]);
             }
         } 
-        // std::cout << "points_1 size: " << points_1.size() << std::endl;
-        // std::cout << "inlier_1 size: " << inlier_1.size() << std::endl;
-        // std::cout << "F: " << F << std::endl;
         
         // check parallax
         double average_parallax;
@@ -197,83 +303,28 @@ int main (int argc, char **argv) {
             std::cout << "determinant(E)!=0: " << cv::determinant(E) << std::endl;
             continue;
         }
-        std::cout << "determinant(E): " << cv::determinant(E) << std::endl;
+        // std::cout << "determinant(E): " << cv::determinant(E) << std::endl;
         // decompose [R t] from E
-        // svd
-        cv::Mat u,w,vt;
-        cv::SVD::compute(E, w, u, vt);
-        // check equality
-        double ratio = fabs(w.at<double>(0) / w.at<double>(1));
-        if (ratio > 1.0) ratio = 1.0 / ratio;
-        if (ratio <0.7) {
-            std::cout << "sigular values are too far away: " << ratio << std::endl;
-            continue;
-        } 
-        std::cout << "ratio: " << ratio << std::endl;
-        cv::Mat W = (cv::Mat_<double>(3,3) << 0, -1, 0, 1, 0, 0, 0, 0, 1);
-        cv::Mat R0 = u * W * vt;
-        if (cv::determinant(R0) < 0)
-            R0 = -R0;
-        cv::Mat R1 = u * W.t() * vt;
-        if (cv::determinant(R1) < 0)
-            R1 = -R1;
-        cv::Mat t0 = u.col(2);
-        cv::Mat t1 = -u.col(2);
-
-        // test four solutions
-        cv::Mat P0 = cv::Mat::eye(3,4,R0.type());
-        std::vector<cv::Mat> candidates;
-        for (int i=0; i<4; i++)
-            candidates.push_back(cv::Mat(3,4,R0.type()));
-        R0.copyTo(candidates[0].colRange(0,3));t0.copyTo(candidates[0].col(3));
-        R0.copyTo(candidates[1].colRange(0,3));t1.copyTo(candidates[1].col(3));
-        R1.copyTo(candidates[2].colRange(0,3));t0.copyTo(candidates[2].col(3));
-        R1.copyTo(candidates[3].colRange(0,3));t1.copyTo(candidates[3].col(3));
-        // candidates[0].colRange(0,3) = R0*1.0; candidates[0].col(3) = t*1.0;
-        // candidates[1].colRange(0,3) = R0*1.0; candidates[1].col(3) = -t*1.0;
-        // candidates[2].colRange(0,3) = R1*1.0; candidates[2].col(3) = t*1.0;
-        // candidates[3].colRange(0,3) = R1*1.0; candidates[3].col(3) = -t*1.0;
+        cv::Mat R, t;
+        DecomposeEssentialMatrix(inlier_1, inlier_2, E, K, R, t);
+                
+        R.copyTo(current_pose.colRange(0,3).rowRange(0,3));
+        t.copyTo(current_pose.col(3).rowRange(0,3));
         
-        // cv::Mat P1(3,4,R0.type()), P2(3,4,R0.type()), P3(3,4,R0.type()), P4(3,4,R0.type());    
-        // std::cout << "R0: " << R0 << std::endl;
-        // std::cout << "R1: " << R1 << std::endl;
-        // std::cout << "t: " << t0 << std::endl;
-        // std::cout << "P1: " << candidates[0] << std::endl;
-        // std::cout << "P2: " << candidates[1] << std::endl;
-        // std::cout << "P3: " << candidates[2] << std::endl;
-        // std::cout << "P4: " << candidates[3] << std::endl;
-        
-        std::vector<int> num_good(4,0);
-
-        for (int i=0; i<4; i++) {            
-            num_good[i] = CheckGoodPoint(inlier_1, inlier_2, K, P0, candidates[i]);
-            std::cout << "num_good" << i << ": " << num_good[i] << std::endl;
-        }
-
-        cv::Mat camera_matrix;
-        std::vector<int>::iterator result = std::max_element(num_good.begin(), num_good.end());
-        int max_position = std::distance(num_good.begin(), result);
-        camera_matrix = candidates[max_position];
-
-        cv::Mat R, trans;
-        double focal = 517.3;
-        cv::Point2d pp(318.6, 255.3); 
-        cv::recoverPose(E, inlier_1, inlier_2, R, trans, focal, pp);
-        cv::Mat gt(3, 4, R.type());
-        gt.colRange(0,3) = R * 1.0;
-        gt.col(3) = trans * 1.0;
-        std::cout << "estimated: " << camera_matrix << std::endl;
-        std::cout << "gt: " << gt << std::endl;
+        current_pose *= last_pose;
+        // std::cout << "R type: " << R.type() << std::endl;
+        // reproject point cloud into the current frame, triangulate unseen points  
+        TriangulateUnseenPoints(inlier_1, inlier_2, point_cloud, last_pose, current_pose, K, width, height); 
 
         cv::Mat output_img = DrawEpilines(last_frame, inlier_1, F, frame, inlier_2);
         cv::imwrite("../images/epiline"+std::to_string(image_index++)+".jpg", output_img);
-
         cv::Mat img_matches;
-        cv::drawMatches(last_frame, last_key_points, frame, key_points, inlier_matches, img_matches, cv::Scalar(255, 0, 0), cv::Scalar(255, 0, 0));
+        cv::drawMatches(last_frame, last_key_points, frame, key_points, inlier_matches, img_matches, cv::Scalar(255, 0, 0), cv::Scalar(255, 0, 0));          
         
         last_frame = frame;
         last_descriptors = descriptors;
         last_key_points = key_points;
+        last_pose = current_pose;
         
         // cv::imshow("image matches", output_img);
         // cv::imshow("image matches", output_img);
